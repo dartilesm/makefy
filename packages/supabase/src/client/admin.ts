70ff7d55fd9304b4333312d10cad1ba3e3f54a00
Diego Artiles <diego@dartiles.dev>
2024-11-10 17:00:18 -0300

    feat(packages/supabase): add the supabase admin function

---
+++ packages/supabase/src/client/admin.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../types";
+
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+}
+
+if (!process.env.SUPABASE_ADMIN) {
+  throw new Error("Missing env.SUPABASE_ADMIN");
+}
+
+export function createSupabaseAdmin() {
+  return createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_ADMIN!,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    },
+  );
+}
