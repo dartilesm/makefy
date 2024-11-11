ad58d88b0ee7ce04903d18e51f76778c09ac8184
Diego Artiles <diego@dartiles.dev>
2024-11-11 20:55:10 -0300

    refactor(packages/supabase): add OR type to the custom SupabaseClient type

--- packages/supabase/src/types/supabase.ts
+++ packages/supabase/src/types/supabase.ts
@@ -1,7 +1,8 @@
 import { createServerClient } from "@supabase/ssr";
 import type { Database } from "@makify/supabase/types/database";
+import { createClient } from ".";
 
 export type * from "@supabase/supabase-js";
-export type SupabaseClient<T extends Database = Database> = ReturnType<
-  typeof createServerClient<T>
->;
+export type SupabaseClient<T extends Database = Database> =
+  | ReturnType<typeof createServerClient<T>>
+  | ReturnType<typeof createClient<T>>;
