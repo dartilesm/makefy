67c990f2d3461b93c463fa2d8b4d2bf14279e2cc
Diego Artiles <diego@dartiles.dev>
2024-11-24 00:03:57 -0300

    feat(apps/text-tools): add the makefy favicon

--- apps/text-tools/app/layout.tsx
+++ apps/text-tools/app/layout.tsx
@@ -11,6 +11,26 @@ export const metadata: Metadata = {
   title: "Text Tools",
   description:
     "A collection of AI-powered tools for text generation and manipulation",
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        url: "/icon1.svg",
+        media: "(prefers-color-scheme: dark)",
+        type: "image/svg+xml",
+      },
+      {
+        rel: "icon",
+        url: "/icon2.svg",
+        media: "(prefers-color-scheme: light)",
+        type: "image/svg+xml",
+      },
+      {
+        rel: "apple-touch-icon",
+        url: "/icon1.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
