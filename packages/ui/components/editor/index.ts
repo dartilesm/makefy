5d0d6484f04859dde892f97fa86d794021824b8b
Diego Artiles <diego@dartiles.dev>
2025-04-06 11:37:50 -0300

    feat(ui): Implement Lexical-based editor component with customizable themes and plugins

---
+++ packages/ui/components/editor/index.ts
@@ -0,0 +1,10 @@
+/* Themes */
+export * from "./themes/editor-theme";
+
+/* Components */
+export * from "./editor-ui/content-editable";
+export * from "./editor";
+export * from "./plugins";
+
+/* Nodes */
+export * from "./nodes";
